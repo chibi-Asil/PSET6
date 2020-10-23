2dab9c243fa7c9c060b3905843d7d51b770389d5
chibi-Asil <32022127+chibi-Asil@users.noreply.github.com>
2020-10-23 15:41:28 -0400

    Update DNA.py

--- DNA.py
+++ DNA.py
@@ -1,4 +1,3 @@
-# Assignment Location: https://cs50.harvard.edu/x/2020/psets/6/dna/#:~:text=python%20dna.py%20databases/large.csv%20sequences/18.txt
 # Implement a program that identifies a person based on their DNA, per the below.
 from cs50 import get_float, get_int, get_string
 # Documentation for argparse can be found at: https://docs.python.org/3.3/library/argparse.html
@@ -15,12 +14,10 @@ import re
 
 def main():
     i = compute()
-    j = comparison_str(STR_string, csv_files)
+    ## j = comparison_str(STR_string, csv_files) // Turns out, I didn't need to call this
 
 # We will need to do the compute first since it is step 2
 def compute():
-    # We should create a str_sequence dictionary so that it would be populate
-    str_sequence = {}
     # The program should require as its first command-line argument the name of a CSV file containing the STR counts for a list of individuals and should require as its second command-line argument the name of a text file containing the DNA sequence to identify.
     if len(argv) != 3:
     # To open the file https://www.pythonforbeginners.com/files/reading-and-writing-files-in-python
@@ -29,18 +26,31 @@ def compute():
 
     # If they enter in the correct command, it should open the file
     # Documentation can be found here: https://www.pythonforbeginners.com/files/reading-and-writing-files-in-python
-    with open(argv[2], "r") as csv_file:
+    databases_csv = argv[2]
+    with open(databases_csv) as csv_file:
         if csv_file.mode != "r":
         # If the file is not a readable file, it should close let the user know. And it should be closed
             print(f"{argv[2]} can not be read.\n")
             exit(1)
         # If it works, it should open
-        csv_files = csv_file.read()
+        csv_files = csv.reader(csv_file)
+        # print(csv_files)
         for row in csv_files:
-            str_list = row
+            print(row)
+            # str_list = row
+            #print(str_list) // It prints out the letter and not the actual string
+    
+    # We will store the str_list into a list
+    #RNA = str_list[0]
+    # print(RNA)
+    
+    # We should create a str_sequence dictionary so that it would be populate
+    str_sequence = {}
+    # A second empty dictionary so that we can copy over the STR values so we can do a side-by-side comparison
+    empty_sequence = {}
 
     # The second file that should be opened should be the database_csv
-    with open(argv[1], newline='') as database_csv:
+    with open(argv[1], "r") as database_csv:
         # If it works, it should open
         database_reader = csv.reader(database_csv)
         # We want to load up the STR to analyze them properly
@@ -48,33 +58,45 @@ def compute():
         database_csv.seek(0)
         header = list(next(database_reader))
         header.pop(0)
+        # print(header) #// Testing to make sure it works
         
     # For each of the STRs (from the first line of the CSV file), your program should compute the longest run of consecutive repeats of the STR in the DNA sequence to identify.
     # We started with header[1:] because the first column is name
-        for STR_string in header[1::]:
+        for STR_string in header[1:]:
         # By doing this, we will populate the empty dictionary with the STR and the amount of STR
+        # We are copying the list into the dictionary and then we will link and compare the the two    
+            empty_sequence[STR_string] = 1
+            #print(empty_sequence)
+            
             str_sequence[STR_string] = comparison_str(STR_string, csv_files)
-
-    # Save STR count in a dictionary
+            #print(str_sequence)
+        
+        #for item in header:
+            #empty_sequence[item] = 1
+            #print(empty_sequence)
+            
     # If the STR counts match exactly with any of the individuals in the CSV file, your program should print out the name of the matching individual.
-    with open(argv[1], newline = '') as file:
+    with open(argv[1], newline = ' ') as file:
         csv_dict = csv.DictReader(file)
         for row in csv_dict:
             STR = 0
             for STR_string in str_sequence:
                 # int(x) takes the string x and turns it into the integer
-                if str_sequence[STR_string] == int(row[STR_string]):
+                if str_sequence[STR_string[1:]] == int(row[STR_string]):
                 # Testing to see how what would work
                 ##print(row[STR_string])
                 ##print(int(STR_string))
                 ##print(int(STR_string))
                     STR += 1
+                    #print(str_sequence[STR_string[1:]])
+                    #print(STR)
+                    #print(int(row[STR_string]))
             # s[i:j] in Python takes the string s, and returns the substring wth all characters from the ith character up to , but not including the jth
-            if STR == len(str_sequence):
-                print(row['name'])
-                exit(0)
-        else:
-            print("No match.\n")
+            #if STR == len(str_sequence):
+                #print(row['name'])
+                #exit()
+        #else:
+            #print("No match.\n")
 
 
 def comparison_str(STR_string, csv_files):
@@ -87,20 +109,15 @@ def comparison_str(STR_string, csv_files):
     max_counter = 0
 
     # To find out the sequence and pattern, we will use regex - both for search and athe match
-    
-    prog = re.compile(r'STR_string (\d+\.\d+\.\d+\.\d+)\s(\w+).*?\s(\w+)$')
-    result = prog.finditer(csv_files)
+    repeating = re.findall(rf"(?:{STR_string}){{2,}}", csv_files)
 
     # According to the previous aforementioned stack over-flow, it recommends that we do a result(begin) - results(end)
-    #for results in result:
-        #counter = results(end) - results(start)
-    # To ensure that max_counter is the largest value, we need to make those two equivalent to one another
-        #if counter > max_counter:
-            #max_counter = counter
-        # return the number of times the pattern repeats itself for the longest repeat
-            #j = max_counter/len(STR_string)
-    j = max((len(seq) for seq in prog.findall(csv_files)), default = 0) // len(prog)
-    return j
+    # The code for the findall is: https://stackoverflow.com/questions/62916456/how-to-find-the-max-number-of-times-a-sequence-of-characters-repeats-consecutiv
+    for matches in repeating:
+        counter = len(matches) / len(repeating)
+        if counter > max_counter:
+            max_counter = counter
+    return int(max_counter)
 
 
 
